79575a704229a2c173fbfb5a12690ee2a0fd4784
Moira Leon <moira.leon2@gmail.com>
2021-07-30 12:24:12 -0600

    trying to animate rocket

--- server/index.js
+++ server/index.js
@@ -7,10 +7,29 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../tictacjs.html'));
 });
 
-// function yowza {
-//   alert('yowza')
-// }
+function spaceCompliment() {
+  alert("yowza")
+}
+
+document.getElementById("doggo").addEventListener("mouseover", spaceCompliment);
 // document.getElementById('doggo').onmouseover
+
+// var id = null;
+// function myMove() {
+//   var elem = document.getElementById("rocket");
+//   var pos = 0;
+//   clearInterval(id);
+//   id = setInterval(frame, 10);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + 'px';
+//       elem.style.left = pos + 'px';
+//     }
+//   }
+// }
 
 const port = process.env.PORT || 4000
 
